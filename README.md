ed593ed2415e45b59db3969fd0a3e05c65564989
MartinAguilarR <martin.aguilarr@utem.cl>
2021-07-19 15:17:09 +0200

    actualización

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Practica_Semana_10
+
+El desafío de esta semana es realizar un scrap para la web Yahoo Finance, con respecto a las empresas que componen el 
+IPSA. 
+
+Se buscó generar un data que contuviese información relevante para el desarrollo de una investigación.
+
--- Scrap Yahoo.py
+++ Scrap Yahoo.py
@@ -16,36 +16,39 @@ chrome_options.add_argument("--window-size=1920x1080")
 driver = webdriver.Chrome(options=chrome_options,
                           executable_path="C:\\Program Files (x86)\\chromedriver.exe")
 
-url = 'https://es.finance.yahoo.com/quote/COPEC.SN?p=COPEC.SN&.tsrc=fin-srch'
+# Definicion de url
+url = 'https://es.finance.yahoo.com/quote/COPEC.SN/key-statistics?p=COPEC.SN'
 
-# Información asociada a la tabla
+# Informacion asociada a la tabla
 driver.get(url)
 print(driver.title)
 time.sleep(8)
 
+# Estructura de data
 data1 = {}
 df1 = pd.DataFrame(
     columns=['Info'])  # Nombres de los header de tabla
 
-rows = len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr"))
-cols = len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[1]/td"))
+rows = len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[1]/div/div/div/div/table/tbody/tr"))
+cols = len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[1]/div/div/div/div/table/tbody/tr[2]/td"))
+
 print(rows)
 print(cols)
 
-# Considerar Alinear elementos dentro de 'data['']' según definición de headers
+# Considerar Alinear elementos dentro de 'data['']' segun definicion de headers
 # PERMITE EXTRAER HASTA ZAMBIA, LUEGO EL MODELO SE QUIEBRA POR ESTRUCTURA DE TABLA PRESENTE AL FINAL.
 
 # Tabla
 
 data = {}
-df = pd.DataFrame(columns=['Contenido', 'Valores'])
+df = pd.DataFrame(columns=['Medidas de valorizacion', 'Valores'])
 
-for r in range(160, rows + 1):
-    data['Contenido'] = driver.find_element_by_xpath(
-        "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div[2]/div[1]/table/tbody/tr[" + str(r) + "]/td[1]").text
-    data['Valores'] = driver.find_element_by_xpath(
-        "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div[2]/div[1]/table/tbody/tr[" + str(r) + "]/td[2]").text
-    df = df.append(data, ignore_index=True)
-    print(df)
-    df.to_csv('Tabla Copec')
+for r in range(1, rows + 1):
+        data['Medidas de valorizacion'] = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[1]/div/div/div/div/table/tbody/tr["+str(r)+"]/td[1]").text
+        data['Valores'] = driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[1]/div/div/div/div/table/tbody/tr["+str(r)+"]/td[2]").text
+        df = df.append(data, ignore_index=True)
+        print(df)
+        df.to_csv('Tabla Copec')
 
